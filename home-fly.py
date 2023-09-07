8f4585a0392570cebb37a04460c1724d7cef08e1
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2023-09-07 05:13:58 +0200

    changed tc ext sensor

--- home-fly.py
+++ home-fly.py
@@ -19,7 +19,8 @@ TOPIC_HOME_FLY_CMND_DISPLAY_SCROLL = TOPIC_HOME_FLY_CMND + "DisplayScrollText"
 
 DISPLAY_LEN = 4
 POWER_METER_EVENT = "sensor.total_watt"
-TC_EXTERNAL_ID = "sensor.ewelink_th01_b0071325_temperature"
+# TC_EXTERNAL_ID = "sensor.ewelink_th01_b0071325_temperature"   guasto 4.9.2023
+TC_EXTERNAL_ID = "sensor.sonoff_th_am2301_temperature"
 METEO_EVENT = "weather.casatorino2022"
 METEO_STATE = METEO_EVENT
 BOILER_STATE = "switch.boiler"
